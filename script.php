c7a3dc6628ac8b5c1698b7b78e2a4f4a4c483725
Pavel Fomin <webdev.fominpa@gmail.com>
2024-10-05 22:54:11 +0300

    replace script to new file

---
+++ script.php
@@ -0,0 +1,9 @@
+<?php
+	define('TG_TOKEN', '7505622461:AAH8y1PWhOWNpkLab7BL9wuBxuBDvvE5Toc');
+	define('MY_ID', '1852081635');
+
+	$url = 'https://api.telegram.org/bot'. TG_TOKEN .'/';
+	//header('Location: ' . $url);
+	$return = file_get_contents($url . 'getMe');
+	print_r($return);
+?>

b73d32d656f9dc411742251ef18fe8bbf3b4ac36
Pavel Fomin <webdev.fominpa@gmail.com>
2024-10-05 19:00:36 +0300

    Test sendMessage with 2 params

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 	define('TG_TOKEN', '7505622461:AAH8y1PWhOWNpkLab7BL9wuBxuBDvvE5Toc');
-	define('MY_ID', '#1852081635');
-	$url = 'https://api.telegram.org/bot'. TG_TOKEN .'/getMe' ;
+	define('MY_ID', '1852081635');
+	$url = 'https://api.telegram.org/bot'. TG_TOKEN .'/sendMessage?chat_id=' . MY_ID . '&text=фром сервер';
 	header('Location: ' . $url);
 ?>
